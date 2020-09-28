d0029c9bc5ddd714a6d76bd7d91fc67dcfbcdc57
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2020-09-28 11:31:53 +0200

    :recycle: update lifecycle hooks style

--- js/app.js
+++ js/app.js
@@ -5,26 +5,26 @@ const vm = new Vue({
     data: {
         title: 'Hello ciné'
     },
-    beforeCreate: function () {
+    beforeCreate() {
         // Les data ne sont pas encore renseignées 🛑
         console.log('beforeCreate', this.title, this.$el); // => undefined
     },
-    created: function () {
+    created() {
         // Les data sont renseignées ✅
         console.log('created', this.title, this.$el); // => 'Hello ciné'
     },
-    beforeMount: function () {
+    beforeMount() {
         // this.$el = element avec les moustaches {{...}} dans le template
         console.log('beforeMount', this.title, this.$el);
     },
-    mounted: function () {
+    mounted() {
         // this.$el = element avec les valeurs interprétées dans le template
         console.log('mounted', this.title, this.$el);
     },
-    beforeDestroy: function () {
+    beforeDestroy() {
         console.log('beforeDestroy', this.title, this.$el);
     },
-    destroyed: function () {
+    destroyed() {
         console.log('destroyed', this.title, this.$el);
     },
 });
